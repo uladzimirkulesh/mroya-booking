2cc4ed541937bc73818f5597cafb9b56866c3282
Uladzimir Kulesh <u.a.kulesh@gmail.com>
2026-05-10 07:27:19 +0200

    working

---
+++ patterns/footer-contrast.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Default footer (contrast)
+ * Slug: mroya-booking/footer-contrast
+ * Description: A default footer section.
+ * Categories: footer
+ * Template Types: footer
+ * Post Types: wp_template, wp_template_part
+ * Viewport Width: 1440
+ *
+ * @package Mroya
+ * @since Mroya 1.0.0
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x( 'Footer Default', 'Name for the Default footer pattern', 'mroya' ); ?>"},"className":"footer__inner has-global-padding is-style-section-contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group footer__inner has-global-padding is-style-section-contrast">
+	<!-- wp:pattern {"slug":"mroya/footer-links"} /-->
+</div>
+<!-- /wp:group -->
